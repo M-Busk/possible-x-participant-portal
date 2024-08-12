aaff62ed44619007b9d3071ed99a00a2f2d84339
cengiz-yazici <cengiz.yazici@capgemini.com>
2024-08-12 15:27:24 +0200

    added changes from pr review

--- edc-orchestrator/build.gradle.kts
+++ edc-orchestrator/build.gradle.kts
@@ -1,13 +1,12 @@
 
 plugins {
-	`java-library`
+	`java`
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
-
 }
 
 group = "eu.possible-x"
-version = ""
+version = "0.0.1"
 
 java {
 	toolchain {
