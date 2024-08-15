570ef8fa4d2ffc368436b0e09481bf19acfe3074
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2024-08-15 15:59:52 +0200

    Write unit tests for  ui

--- consumer-provider-frontend/src/app/views/offer/provide/provide.component.spec.ts
+++ consumer-provider-frontend/src/app/views/offer/provide/provide.component.spec.ts
@@ -1,23 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { ProvideComponent } from './provide.component';
+import { ApiService } from '../../../services/mgmt/api/api.service';
 
 describe('ProvideComponent', () => {
   let component: ProvideComponent;
   let fixture: ComponentFixture<ProvideComponent>;
+  let apiService: jasmine.SpyObj<ApiService>;
 
   beforeEach(async () => {
+    const apiServiceSpy = jasmine.createSpyObj('ApiService', ['getHealth', 'createOffer']);
+
     await TestBed.configureTestingModule({
-      declarations: [ProvideComponent]
+      declarations: [ProvideComponent],
+      providers: [
+        { provide: ApiService, useValue: apiServiceSpy }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ProvideComponent);
     component = fixture.componentInstance;
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getHealth on apiService when getHealth is called', () => {
+    apiService.getHealth.and.returnValue(of({ status: 'healthy' }));
+    component.getHealth();
+    expect(apiService.getHealth).toHaveBeenCalled();
+  });
+
+  it('should call createOffer on apiService when createOffer is called', () => {
+    apiService.createOffer.and.returnValue(of({ id: '123' }));
+    component.createOffer();
+    expect(apiService.createOffer).toHaveBeenCalled();
   });
 });
