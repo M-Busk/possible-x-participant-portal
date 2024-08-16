b9e743be1501f4dc2398bd78a69521ec5115b04a
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2024-08-16 11:18:21 +0200

    Revert "Adjust ui tests"
    This reverts commit 2af2db6143586f576daf3e46eecf5736ed4d862c.

--- consumer-provider-frontend/src/app/containers/default-layout/default-layout.component.spec.ts
+++ consumer-provider-frontend/src/app/containers/default-layout/default-layout.component.spec.ts
@@ -1,9 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NavbarModule, GridModule, CollapseModule, NavModule, FooterModule } from '@coreui/angular';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, ActivatedRoute } from '@angular/router';
+
 import { DefaultLayoutComponent } from './default-layout.component';
-import { of } from 'rxjs';
 
 describe('DefaultLayoutComponent', () => {
   let component: DefaultLayoutComponent;
@@ -11,17 +8,7 @@ describe('DefaultLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DefaultLayoutComponent],
-      imports: [ NavbarModule, GridModule, CollapseModule, NoopAnimationsModule, NavModule, RouterModule, FooterModule ],
-      providers: [
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            params: of({}), // Mock route parameters
-            snapshot: { paramMap: { get: () => null } } // Mock snapshot
-          }
-        }
-      ]
+      declarations: [DefaultLayoutComponent]
     })
     .compileComponents();
 
