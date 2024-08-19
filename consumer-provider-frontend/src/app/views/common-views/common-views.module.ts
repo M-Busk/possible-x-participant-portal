3df64f379086dd2a43394662581512a84c754946
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2024-08-19 17:53:12 +0200

    Add status message component

---
+++ consumer-provider-frontend/src/app/views/common-views/common-views.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { StatusMessageComponent } from './status-message/status-message.component';
+
+@NgModule({
+  declarations: [StatusMessageComponent],
+  imports: [],
+  exports: [StatusMessageComponent],
+})
+
+export class CommonViewsModule {}
--- consumer-provider-frontend/src/app/views/offer/provide/provide.component.ts
+++ consumer-provider-frontend/src/app/views/offer/provide/provide.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ApiService } from '../../../services/mgmt/api/api.service'
+import { StatusMessageComponent } from '../../common-views/status-message/status-message.component';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-provide',
@@ -7,10 +9,15 @@ import { ApiService } from '../../../services/mgmt/api/api.service'
   styleUrl: './provide.component.scss'
 })
 export class ProvideComponent {
+  @ViewChild('offerCreationStatusMessage') private offerCreationStatusMessage!: StatusMessageComponent;
   offerType: string = "";
   offerName: string = "";
 
   constructor(private apiService: ApiService) {}
+
+  protected hideAllMessages(){
+    this.offerCreationStatusMessage.hideAllMessages();
+  }
 
   getHealth(): void {
     this.apiService.getHealth().subscribe(response => {
@@ -18,9 +25,14 @@ export class ProvideComponent {
     });
   }
 
-  createOffer(): void {
-    this.apiService.createOffer().subscribe(response => {
+  protected async createOffer() {
+    this.offerCreationStatusMessage.showInfoMessage();
+
+    this.apiService.createOffer().then(response => {
       console.log(response);
+      this.offerCreationStatusMessage.showSuccessMessage(`ID: ${response.id}`);
+    }).catch((e: HttpErrorResponse) => {
+      this.offerCreationStatusMessage.showErrorMessage(e.error.message);
     });
   }
 }
