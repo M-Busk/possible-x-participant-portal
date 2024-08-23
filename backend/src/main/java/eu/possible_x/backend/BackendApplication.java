f8b9f2f835226e658fadc707a1d3277bdf947fa2
M-Busk <125284318+M-Busk@users.noreply.github.com>
2024-08-23 10:02:30 +0200

    move main class to root again

--- backend/src/main/java/eu/possible_x/backend/BackendApplication.java
+++ backend/src/main/java/eu/possible_x/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package eu.possible_x.backend.application.configuration;
+package eu.possible_x.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
