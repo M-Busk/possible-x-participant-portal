477fd5e1a89b2bd96093d8ce30ff3339773be9a1
M-Busk <125284318+M-Busk@users.noreply.github.com>
2024-10-25 14:05:01 +0200

    fix test

--- backend/src/test/java/eu/possiblex/participantportal/business/control/ProviderServiceTest.java
+++ backend/src/test/java/eu/possiblex/participantportal/business/control/ProviderServiceTest.java
@@ -158,7 +158,7 @@ class ProviderServiceTest {
         assertTrue(serviceOfferingId.matches(
             "urn:uuid:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"));
         assertTrue(dataResource.getId()
-            .matches("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"));
+            .matches("urn:uuid:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"));
         assertEquals(serviceOfferingId, dataResource.getExposedThrough().getId());
         //check if assetId exists and provider url is set correctly
         assertTrue(pxExtSoCs.getAssetId()
