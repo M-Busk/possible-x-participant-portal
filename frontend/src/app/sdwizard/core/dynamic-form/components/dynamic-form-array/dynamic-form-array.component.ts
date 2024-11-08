692f62627f111422b2f9fd69af79cbdbd87d0cf7
cengiz-yazici <150052554+cengiz-yazici@users.noreply.github.com>
2024-11-08 15:33:26 +0100

    Feature/pi 629 add help text (#74)
    * [PI-629][PI-637][PI-638][PI-639] replaced label form, changed policy instruction text and implemented accordions for policy hints
    
    * [PI-629][PI-638][PI-639] unordered list are now shown correctly
    
    * [PI-629][PI-646] removed policy tooltips
    
    * [PI-629] implemented feedback
    
    * [PI-629] more feedback implementation

--- frontend/src/app/sdwizard/core/dynamic-form/components/dynamic-form-array/dynamic-form-array.component.ts
+++ frontend/src/app/sdwizard/core/dynamic-form/components/dynamic-form-array/dynamic-form-array.component.ts
@@ -22,42 +22,6 @@ export class DynamicFormArrayComponent implements OnInit {
   validator: ValidatorFn[];
   validationService: ValidationControlService = new ValidationControlService();
 
-  serviceOfferingPolicyTooltip: string = 'Below you have the option of entering a policy for the use and management of this service. \n' +
-    'The policy should also include the restrictions set out in the predefined policy class. \n' +
-    '\n' +
-    'This policy is neither technically enforced nor is its compliance monitored. However, the user of the service undertakes to comply with the guidelines listed here.\n' +
-    '\n' +
-    'Please take the following points into account:\n' +
-    '\n' +
-    '1. Access rights: Who is allowed to use the data transfer service? (e.g. only certain departments or roles)\n' +
-    '\n' +
-    '2. Terms of use: Under what conditions may data be transferred? (e.g. only encrypted transfers, no transfer of sensitive data without authorization)\n' +
-    '\n' +
-    '3. Security measures: What security precautions need to be taken? (e.g. use of VPNs, two-factor authentication)\n' +
-    '\n' +
-    '4. Logging and monitoring: How is the use of the service logged and monitored? (e.g. regular review of transmission logs)\n' +
-    '\n' +
-    '5. Data integrity: What measures are taken to ensure the integrity of the transmitted data? (e.g. checksums, error correction procedures)\n' +
-    '\n' +
-    '6. Responsibilities: Who is responsible for managing and monitoring compliance with these policies? (e.g. IT department, security officer)';
-  dataResourcePolicyTooltip: string = 'Below you have the option of entering a policy for the use and management of your data. \n' +
-    '\n' +
-    'This policy is neither technically enforced nor is compliance checked. However, the user of your data undertakes to comply with the guidelines set out here.\n' +
-    '\n' +
-    'The policy for the data transfer service and the technically enforced policy can be entered in the following fields.\n' +
-    '\n' +
-    'Take the following points into account:\n' +
-    '\n' +
-    '1. Access rights: Who is allowed to access this data resource? (e.g. only certain departments or roles)\n' +
-    '\n' +
-    '2. Terms of use: How may the data be used? (e.g. only for internal analyses, not to be passed on to third parties)\n' +
-    '\n' +
-    '3. Security measures: What security precautions need to be taken? (e.g. encryption, regular security checks)\n' +
-    '\n' +
-    '4. Data retention: How long should the data be retained and when should it be deleted? (e.g. retention for 5 years, then secure deletion)\n' +
-    '\n' +
-    '5. Responsibilities: Who is responsible for managing and monitoring compliance with these policies? (e.g. IT department, data protection officer)';
-
 
   constructor(public translate: TranslateService) {
   }
@@ -111,12 +75,4 @@ export class DynamicFormArrayComponent implements OnInit {
     return Utils.hasRequiredField(control);
   }
 
-  getTooltip(name: string): string | null {
-    if (name === 'Service Offering Policy') {
-      return this.serviceOfferingPolicyTooltip;
-    } else if (name === 'Data Resource Policy') {
-      return this.dataResourcePolicyTooltip;
-    }
-    return null;
-  }
 }
