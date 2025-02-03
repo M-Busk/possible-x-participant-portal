140dbd79bc6bda1810e7867d583885e97e2d88e2
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2025-02-03 12:04:01 +0100

    Fix UI test

--- frontend/src/app/views/attribution-document/attribution-document.component.spec.ts
+++ frontend/src/app/views/attribution-document/attribution-document.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AttributionDocumentComponent } from './attribution-document.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
-describe('HomeComponent', () => {
+describe('AttributionDocumentComponent', () => {
   let component: AttributionDocumentComponent;
   let fixture: ComponentFixture<AttributionDocumentComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AttributionDocumentComponent]
+      declarations: [AttributionDocumentComponent],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
 
